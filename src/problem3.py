d5d307c11bf1ac8364ef9925c45d8f2ba8b9fd96
strainlw <strainlw@rose-hulman.edu>
2019-01-31 20:29:51 -0500

    DONE

--- src/problem3.py
+++ src/problem3.py
@@ -3,9 +3,9 @@ Exam 3, problem 3.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Loki Strain.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -185,6 +185,13 @@ def problem3(sequence):
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    count = 0
+    for k in range(len(sequence)):
+        if is_prime(sequence[k]) is True:
+            count = count + 1
+            sequence[k] = sequence[k]+3
+    return count
 
 
 ###############################################################################
