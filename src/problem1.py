7c00008c16c7147f0675378163eca12708ca6187
strainlw <strainlw@rose-hulman.edu>
2019-01-31 20:26:20 -0500

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -3,9 +3,9 @@ Exam 3, problem 1.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Loki Strain.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -139,6 +139,26 @@ def problem1(n):
     # and THEN try the full-credit version.
     # As always, CONTINUE to the next problem if you are STUCK on this one.
     # -------------------------------------------------------------------------
+    for k in range(1, n):
+
+        if fibonacci(k+1) >= n:
+            return fibonacci(k)
+    else:
+        return 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ###############################################################################
